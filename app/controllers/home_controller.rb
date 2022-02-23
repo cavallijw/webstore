3d419cfcbc887b689c5f9aff8d605d9b72087eff
Joey Cavalli <cavallijw1199@gmail.com>
2022-02-23 10:44:18 -0500

    added cart function

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,19 @@
 class HomeController < ApplicationController
     def index
+        if session[:cart].nil?
+            session[:cart] = []
+        end
         @products = Product.all
+        @cart = session[:cart]
     end
+    def buy
+        if session[:cart].nil?
+            session[:cart] = []
+        end
+        product = Product.find(params[:id])
+        session[:cart].append(product)
+        redirect_to :root
+    end
+    def checkout
+        @cart = session[:cart]
 end
