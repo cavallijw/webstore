56d113b195694e6c6c3cf8eea31aa6b1b52d7815
Joey Cavalli <cavallijw1199@gmail.com>
2022-03-29 10:57:49 -0400

    json

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
     root "home#index"
+    get 'products.json' to 'products#list3'
     get "/products/:id/buy", to: "home#buy", as: "buy"
     get "/checkout", to: "home#checkout", as: "checkout"
 end
